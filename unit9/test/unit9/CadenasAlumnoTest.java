82f0f6162030dd691b4589081460a5efd371bc10
arai86 <felipe.otero@hotmail.es>
2015-05-10 16:48:10 +0200

    test unit 9

--- unit9/test/unit9/CadenasAlumnoTest.java
+++ unit9/test/unit9/CadenasAlumnoTest.java
@@ -13,106 +13,193 @@ import static org.junit.Assert.*;
  * @author Felipe
  */
 public class CadenasAlumnoTest {
+    private CadenasAlumno ca;
     
-    public CadenasAlumnoTest() {
+    public CadenasAlumnoTest() 
+    {
+        ca=new CadenasAlumno();
     }
 
     /**
      * Test of sonIguales method, of class CadenasAlumno.
      */
     @Test
-    public void testSonIguales() {
+    public void testSonIguales() 
+    {
+        String cadena1="Alba";
+        String cadena2="Alba";
+        boolean actual=ca.sonIguales(cadena1, cadena2);
+        boolean expected=cadena1.equals(cadena2);
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of esMayor method, of class CadenasAlumno.
      */
     @Test
-    public void testEsMayor() {
+    public void testEsMayor() 
+    {
+        String cadena1="A";
+        String cadena2="C";
+        boolean actual=ca.esMayor(cadena1, cadena2);
+        boolean expected=false;
+        int number =cadena1.compareTo(cadena2);//el compareto da un numero, si es mayor que cero es que cadena1 es mayor que cadena 2;
+        if(number>0)
+        {
+            expected=true;
+        }
+        else
+        {
+            expected=false;
+        }
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of esMenor method, of class CadenasAlumno.
      */
     @Test
-    public void testEsMenor() {
+    public void testEsMenor() 
+    {
+        String cadena1="A";
+        String cadena2="C";
+        boolean actual=ca.esMenor(cadena1, cadena2);
+        boolean expected;
+        int number =cadena1.compareTo(cadena2);//el compareto da un numero, si es mayor que cero es que cadena1 es mayor que cadena 2;
+        if(number<0)
+        {
+            expected=true;
+        }
+        else
+        {
+            expected=false;
+        }
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of comparaIgnorandoMayusculas method, of class CadenasAlumno.
      */
     @Test
-    public void testComparaIgnorandoMayusculas() {
+    public void testComparaIgnorandoMayusculas() 
+    {
+        String cadena1="A";
+        String cadena2="a";
+        boolean actual=ca.comparaIgnorandoMayusculas(cadena1, cadena2);
+        boolean expected=cadena1.equalsIgnoreCase(cadena2);
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of reemplazaTodos method, of class CadenasAlumno.
      */
     @Test
-    public void testReemplazaTodos() {
+    public void testReemplazaTodos() 
+    {
+        String cadena1="hola";
+        String valorAnterior="ho";
+        String valorPosterior="mo";
+        String actual=ca.reemplazaTodos(cadena1, valorAnterior, valorPosterior);
+        String expected=cadena1.replaceAll(valorAnterior, valorPosterior);
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of reemplazaPrimero method, of class CadenasAlumno.
      */
     @Test
-    public void testReemplazaPrimero() {
+    public void testReemplazaPrimero() 
+    {
+        String cadena1="hola";
+        String valorAnterior="ho";
+        String valorPosterior="mo";
+        String actual=ca.reemplazaPrimero(cadena1, valorAnterior, valorPosterior);
+        String expected=cadena1.replaceAll(valorAnterior, valorPosterior);
+        assertEquals(expected, actual);
     }
-
-    /**
-     * Test of muestraSplitLimite method, of class CadenasAlumno.
-     */
-    @Test
-    public void testMuestraSplitLimite() {
-    }
-
-    /**
-     * Test of muestraSplit method, of class CadenasAlumno.
-     */
-    @Test
-    public void testMuestraSplit() {
-    }
-
     /**
      * Test of quitaEspacios method, of class CadenasAlumno.
      */
     @Test
-    public void testQuitaEspacios() {
+    public void testQuitaEspacios() 
+    {
+        String cadena1="       AC";
+        String cadena2="AC";
+        String actual=ca.quitaEspacios(cadena1);
+        String expected=cadena1.trim();
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of convertirMayusculas method, of class CadenasAlumno.
      */
     @Test
-    public void testConvertirMayusculas() {
+    public void testConvertirMayusculas() 
+    {
+        String cadena1="a";
+        String actual=ca.convertirMayusculas(cadena1);
+        String expected=cadena1.toUpperCase();
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of convertirMinusculas method, of class CadenasAlumno.
      */
     @Test
-    public void testConvertirMinusculas() {
+    public void testConvertirMinusculas() 
+    {
+        String cadena1="a";
+        String actual=ca.convertirMinusculas(cadena1);
+        String expected=cadena1.toLowerCase();
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of longitudCadena method, of class CadenasAlumno.
      */
     @Test
-    public void testLongitudCadena() {
+    public void testLongitudCadena() 
+    {
+        String cadena1="A";
+        long actual=ca.longitudCadena(cadena1);
+        long expected=cadena1.length();
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of empiezaCon method, of class CadenasAlumno.
      */
     @Test
-    public void testEmpiezaCon() {
+    public void testEmpiezaCon() 
+    {
+        String letra="r";
+        String cadena1="razon";
+        boolean actual=ca.empiezaCon(cadena1, String.valueOf(letra));
+        boolean expected=false;
+        char aux=cadena1.charAt(0);
+        String letraInicial = new StringBuilder().append("").append(aux).toString();
+        if(letraInicial.equals(letra))
+        {
+            expected=true;
+        }
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of acabaEn method, of class CadenasAlumno.
      */
     @Test
-    public void testAcabaEn() {
+    public void testAcabaEn() 
+    {
+        char letra='n';
+        String cadena1="razon";
+        boolean actual=ca.acabaEn(cadena1, String.valueOf(letra));
+        boolean expected=false;
+        if((cadena1.charAt(cadena1.length()-1))==(char) letra)
+        {
+            expected=true;
+        }
+        assertEquals(expected, actual);
     }
 
     /**
@@ -120,55 +207,34 @@ public class CadenasAlumnoTest {
      */
     @Test
     public void testPosicionPrimeraCadena() {
+        String cadena1 = "Cosa";
+        String cadena2 ="o";
+        long actual=ca.posicionPrimeraCadena(cadena1, cadena2);
+        long expected=cadena1.indexOf(cadena2);
+        assertEquals(expected, actual);
     }
-
-    /**
-     * Test of apareceEn method, of class CadenasAlumno.
-     */
-    @Test
-    public void testApareceEn() {
-    }
-
     /**
      * Test of extraerSubstring method, of class CadenasAlumno.
      */
     @Test
-    public void testExtraerSubstring_String_int() {
+    public void testExtraerSubstring_String_int() 
+    {
+        String cadena1="En un lugar de la mancha";
+        int posicionInicio=5;
+        String actual=ca.extraerSubstring(cadena1, posicionInicio);
+        String expected=cadena1.substring(posicionInicio, cadena1.length());
+        assertEquals(expected, actual);
     }
-
-    /**
-     * Test of extraerSubstring method, of class CadenasAlumno.
-     */
-    @Test
-    public void testExtraerSubstring_3args() {
-    }
-
     /**
      * Test of concatenaCadenas method, of class CadenasAlumno.
      */
     @Test
-    public void testConcatenaCadenas() {
+    public void testConcatenaCadenas() 
+    {
+        String cadena1="En un lugar";
+        String cadena2="De la mancha";
+        String actual=ca.concatenaCadenas(cadena1, cadena2);
+        String expected=cadena1.concat(cadena2);
+        assertEquals(expected, actual);
     }
-
-    /**
-     * Test of sonLetras method, of class CadenasAlumno.
-     */
-    @Test
-    public void testSonLetras() {
-    }
-
-    /**
-     * Test of sonLetras2 method, of class CadenasAlumno.
-     */
-    @Test
-    public void testSonLetras2() {
-    }
-
-    /**
-     * Test of main method, of class CadenasAlumno.
-     */
-    @Test
-    public void testMain() {
-    }
-    
 }
